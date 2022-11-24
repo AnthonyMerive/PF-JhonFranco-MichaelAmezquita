b45ae760025484e2a2eaaa75ce13a180482fed27
David777 vegeta <lola.flores19796@gmail.com>
2022-11-23 22:32:44 -0500

    Casi culminación de proyecto jovenes creaTIvos

---
+++ JavaScript/login.js
@@ -0,0 +1,30 @@
+function obtenerDatos() {
+  let usuario = document.getElementById("usuario").value;
+  let pass = document.getElementById("contraseña").value;
+  let pass2 = document.getElementById("contraseñaConfirmacion").value;
+  if (pass === pass2) {
+    if (pass.length < 6) {
+      Swal.fire({
+        title: "Error!",
+        text: "La contraseña debe tener al menos 6 caracteres",
+        icon: "error",
+        confirmButtonText: "De acuerdo",
+      });
+    } else {
+      Swal.fire({
+        title: "Correcto!",
+        icon: "success",
+        confirmButtonText: "Vamos!",
+      }).then(() => {
+        location.href = "https://sweetalert2.github.io/#download";
+      });
+    }
+  } else {
+    Swal.fire({
+      title: "Error!",
+      text: "Las contraseñas no coinciden",
+      icon: "error",
+      confirmButtonText: "De acuerdo",
+    });
+  }
+}
